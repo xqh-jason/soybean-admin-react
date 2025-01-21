f6450907e1863a3a69454b53bac4311cd510067b
wang <1509326266@qq.com>
2025-01-22 05:57:39 +0800

    feat(projects): 添加home路由

---
+++ src/pages/(base)/home/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  i18nKey: 'route.home',
+  title: 'home'
+};
+
+export const loader = undefined;
+
+export const action = undefined;
+
+export const shouldRevalidate = undefined;
