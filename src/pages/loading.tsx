acac156715210bac76cbc2e728d2ab5877521ddb
wang <1509326266@qq.com>
2025-01-22 22:42:44 +0800

    feat(projects): 添加全局loading组件

---
+++ src/pages/loading.tsx
@@ -0,0 +1,35 @@
+import clsx from 'clsx';
+
+import SystemLogo from '@/components/SystemLogo';
+
+const GlobalLoading = memo(() => {
+  const { t } = useTranslation();
+
+  const loadingClasses = [
+    'left-0 top-0',
+    'left-0 bottom-0 animate-delay-500',
+    'right-0 top-0 animate-delay-1000',
+    'right-0 bottom-0 animate-delay-1500'
+  ];
+
+  return (
+    <div className="fixed-center flex-col">
+      <SystemLogo className="size-128px text-primary" />
+      <div className="my-36px h-56px w-56px">
+        <div className="relative h-full animate-spin">
+          {loadingClasses.map(item => {
+            return (
+              <div
+                className={clsx('absolute w-16px h-16px bg-primary rounded-8px animate-pulse ', item)}
+                key={item}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <h2 className="text-28px text-#646464 font-500">{t('system.title')}</h2>
+    </div>
+  );
+});
+
+export default GlobalLoading;
