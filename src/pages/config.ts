7e6df0b893d83c944317480b5a17774bcc0081b9
wang <1509326266@qq.com>
2025-01-22 22:50:41 +0800

    feat(projects): 集成motion

---
+++ src/pages/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  i18nKey: 'route.root',
+  title: 'root'
+};
+
+export const loader = undefined;
+
+export const action = undefined;
+
+export const shouldRevalidate = undefined;
