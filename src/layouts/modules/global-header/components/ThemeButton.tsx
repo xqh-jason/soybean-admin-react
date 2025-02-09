a0b77f19dcba2f2c315df86e8c94fe615bcd860b
wang <1509326266@qq.com>
2025-02-09 12:35:17 +0800

    feat: 添加头部主题按钮组件

---
+++ src/layouts/modules/global-header/components/ThemeButton.tsx
@@ -0,0 +1,23 @@
+import { openThemeDrawer } from '@/store/slice/app';
+
+const ThemeButton = memo(() => {
+  const { t } = useTranslation();
+
+  const dispatch = useAppDispatch();
+
+  function handleClick() {
+    dispatch(openThemeDrawer());
+  }
+
+  return (
+    <ButtonIcon
+      triggerParent
+      className="px-12px"
+      icon="majesticons:color-swatch-line"
+      tooltipContent={t('icon.themeConfig')}
+      onClick={handleClick}
+    />
+  );
+});
+
+export default ThemeButton;
