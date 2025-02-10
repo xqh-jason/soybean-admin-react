7938bbb9b49f054345069f26ef88afc2ae87089d
wang <1509326266@qq.com>
2025-02-10 10:08:37 +0800

    refactor: 删除无用的文件

--- src/pages/403.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Forbidden = () => {
-  return <div>Forbidden</div>;
-};
-
-export default Forbidden;
--- src/pages/500.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NetError = () => {
-  return <div>NetError</div>;
-};
-
-export default NetError;
