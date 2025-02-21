84da36a8c2711a92132eb948fbeb22fb0e111f5e
ohh <1509326266@qq.com>
2025-02-21 13:42:10 -0800

    feat: 添加首页路由用于转发页面

--- src/pages/config.ts
+++ src/pages/config.ts
@@ -1,4 +1,11 @@
+import { redirect } from 'react-router-dom';
+
 export const config = {
+  constant: true,
   i18nKey: 'route.root',
   title: 'root'
+};
+
+export const loader = () => {
+  return redirect(import.meta.env.VITE_ROUTE_HOME);
 };
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { Navigate } from 'react-router-dom';
+
+const Index = () => {
+  return <Navigate to="/manage/menu" />;
+};
+
+export default Index;
