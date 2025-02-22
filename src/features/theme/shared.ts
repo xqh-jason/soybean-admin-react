97c497e119d53a86ca27d46c6af6e86d9e3b6d6c
Ohh-889 <1509326266@qq.com>
2025-02-22 23:33:45 +0800

    reverse: 回退代码 使themeSetting在生产环境下生效

--- src/features/theme/shared.ts
+++ src/features/theme/shared.ts
@@ -13,7 +13,7 @@ export function initThemeSettings() {
   const isProd = import.meta.env.PROD;
 
   // if it is development mode, the theme settings will not be cached, by update `themeSettings` in `src/theme/settings.ts` to update theme settings
-  // if (!isProd) return themeSettings;
+  if (!isProd) return themeSettings;
 
   // if it is production mode, the theme settings will be cached in localStorage
   // if want to update theme settings when publish new version, please update `overrideThemeSettings` in `src/theme/settings.ts`
