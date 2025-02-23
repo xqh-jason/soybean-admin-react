a019fd0eb13226d3c6256f1cbeed8826835c05c9
Ohh-889 <1509326266@qq.com>
2025-02-23 14:58:53 +0800

    refctor: 删掉无用的代码

--- src/layouts/modules/global-tab/GlobalTab.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const GlobalTab = () => {
-  return <div>GlobalTab</div>;
-};
-
-export default GlobalTab;
--- src/layouts/modules/global-tab/shared.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Filter tabs by id
- *
- * @param tabId
- * @param tabs
- */
-export function filterTabsById(tabId: string, tabs: App.Global.Tab[]) {
-  return tabs.filter(tab => tab.id !== tabId);
-}
