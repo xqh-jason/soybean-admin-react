e951b8e981dc9b4f366b78425a975ab1f21166e3
Ohh-889 <1509326266@qq.com>
2025-03-02 01:19:38 +0800

    chore: 升级vite 路由插件

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -1,4 +1,3 @@
-import react from '@vitejs/plugin-react';
 import type { PluginOption } from 'vite';
 import Inspect from 'vite-plugin-inspect';
 import removeConsole from 'vite-plugin-remove-console';
@@ -12,11 +11,10 @@ import { setupUnPluginIcon } from './unplugin-icon';
 
 export function setupVitePlugins(viteEnv: Env.ImportMeta, buildTime: string) {
   const plugins: PluginOption = [
-    react(),
+    setupElegantRouter(),
     setupAutoImport(viteEnv),
     setupUnocss(viteEnv),
     ...setupUnPluginIcon(viteEnv),
-    setupElegantRouter(),
     Inspect(),
     removeConsole(),
     setupHtmlPlugin(buildTime),
