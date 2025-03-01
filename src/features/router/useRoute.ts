6ca3a0df797170e2d810662c112577b04345629d
Ohh-889 <1509326266@qq.com>
2025-03-02 01:32:24 +0800

    feat: useRoute新增 error的显示

--- src/features/router/useRoute.ts
+++ src/features/router/useRoute.ts
@@ -1,19 +1,22 @@
-import { useMatches } from 'react-router-dom';
+import { useMatches, useRouteError } from 'react-router-dom';
 
 import { parseQuery } from './query';
 
 /** - get route meta */
-export function useRoute<T = unknown>() {
+export function useRoute<T = unknown, Q extends Record<string, string> = Record<string, string>>() {
   const matches = useMatches();
 
   const route = matches.at(-1) as Router.Route<T>;
 
   const { hash, pathname, search } = useLocation();
 
-  const query = parseQuery(search);
+  const query = parseQuery(search) as Q;
+
+  const error = useRouteError();
 
   return {
     ...route,
+    error,
     fullPath: pathname + search + hash,
     matched: matches.slice(1) as Router.Route<T>[],
     query,
