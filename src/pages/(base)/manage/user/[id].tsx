322444c8ef752d2af4ff71514732e799aacfc672
Ohh-889 <1509326266@qq.com>
2025-03-05 00:03:42 +0800

    feat: 添加用户详情页面

---
+++ src/pages/(base)/manage/user/[id].tsx
@@ -0,0 +1,10 @@
+const User = () => {
+  return <div>User</div>;
+};
+
+export const handle = {
+  i18nKey: 'route.(base)_manage_user_[id]',
+  title: '(base)_manage_user_[id]'
+};
+
+export default User;
