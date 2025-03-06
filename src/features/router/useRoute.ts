d56dad8442668fc66a38fc0adcfefe4427a6c341
COBORGA-8FFVDVM\cob <1509326266@qq.com>
2025-03-06 17:17:05 +0800

    optimize: 补充useRoute

--- src/features/router/useRoute.ts
+++ src/features/router/useRoute.ts
@@ -2,24 +2,45 @@ import { useMatches, useRouteError } from 'react-router-dom';
 
 import { parseQuery } from './query';
 
+function usePrevious<T>(value: T): T | null {
+  const ref = useRef<T>(null);
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
+
 /** - get route meta */
 export function useRoute<T = unknown, Q extends Record<string, string> = Record<string, string>>() {
   const matches = useMatches();
 
-  const route = matches.at(-1) as Router.Route<T>;
+  const routes = matches.at(-1) as Router.Route<T>;
 
   const { hash, pathname, search } = useLocation();
 
+  const fullPath = pathname + search + hash;
+
   const query = parseQuery(search) as Q;
 
-  const error = useRouteError();
+  const error = useRouteError() as Error | null;
 
-  return {
-    ...route,
+  const route = {
+    ...routes,
     error,
-    fullPath: pathname + search + hash,
+    fullPath,
+    hash,
     matched: matches.slice(1) as Router.Route<T>[],
+    pathname,
     query,
+    redirect: null,
     search
-  };
+  } as Router.Route<T, Q>;
+
+  const previousRoute = usePrevious(route);
+
+  route.redirect = previousRoute;
+
+  return useMemo(() => route, [route.fullPath]);
 }
