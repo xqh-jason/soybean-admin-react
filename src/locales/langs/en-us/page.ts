44b27b28b82f065fdbb1f06d5650cb27811d608c
Ohh-889 <1509326266@qq.com>
2025-03-20 09:15:49 +0800

    feat: 更新描述的版本

--- src/locales/langs/en-us/page.ts
+++ src/locales/langs/en-us/page.ts
@@ -1,7 +1,7 @@
 const page: App.I18n.Schema['translation']['page'] = {
   about: {
     devDep: 'Development Dependency',
-    introduction: `SoybeanAdmin is an elegant and powerful admin template, based on the latest front-end technology stack, including React18.3, Vite5, TypeScript, ReactRouter6.4,Redux/toolkitand UnoCSS. It has built-in rich theme configuration and components, strict code specifications, and an automated file routing system. In addition, it also uses the online mock data solution based on ApiFox. SoybeanAdmin provides you with a one-stop admin solution, no additional configuration, and out of the box. It is also a best practice for learning cutting-edge technologies quickly.`,
+    introduction: `SoybeanAdmin is an elegant and powerful admin template, based on the latest front-end technology stack, including React19.0, Vite6, TypeScript, ReactRouter7,Redux/toolkitand UnoCSS. It has built-in rich theme configuration and components, strict code specifications, and an automated file routing system. In addition, it also uses the online mock data solution based on ApiFox. SoybeanAdmin provides you with a one-stop admin solution, no additional configuration, and out of the box. It is also a best practice for learning cutting-edge technologies quickly.`,
     prdDep: 'Production Dependency',
     projectInfo: {
       githubLink: 'Github Link',
--- src/locales/langs/zh-cn/page.ts
+++ src/locales/langs/zh-cn/page.ts
@@ -1,7 +1,7 @@
 const page: App.I18n.Schema['translation']['page'] = {
   about: {
     devDep: '开发依赖',
-    introduction: `SoybeanAdmin 是一个优雅且功能强大的后台管理模板，基于最新的前端技术栈，包括 React18.3, Vite5, TypeScript,ReactRouter6.4,Redux/toolkit 和 UnoCSS。它内置了丰富的主题配置和组件，代码规范严谨，实现了自动化的文件路由系统。此外，它还采用了基于 ApiFox 的在线Mock数据方案。SoybeanAdmin 为您提供了一站式的后台管理解决方案，无需额外配置，开箱即用。同样是一个快速学习前沿技术的最佳实践。`,
+    introduction: `SoybeanAdmin 是一个优雅且功能强大的后台管理模板，基于最新的前端技术栈，包括 React19.0, Vite6, TypeScript,ReactRouter7,Redux/toolkit 和 UnoCSS。它内置了丰富的主题配置和组件，代码规范严谨，实现了自动化的文件路由系统。此外，它还采用了基于 ApiFox 的在线Mock数据方案。SoybeanAdmin 为您提供了一站式的后台管理解决方案，无需额外配置，开箱即用。同样是一个快速学习前沿技术的最佳实践。`,
     prdDep: '生产依赖',
     projectInfo: {
       githubLink: 'Github 地址',
